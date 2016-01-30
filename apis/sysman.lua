a2582773e9dfd0d151791be3d02af36ed6a92d78
apemanzilla <dominic@marcuse.us>
2016-01-30 09:28:01 -0500

    Start on sysman API

---
+++ apis/sysman.lua
@@ -0,0 +1,14 @@
+--[[ System management API ]]
+--[[ Contains various features for controlling the system - i.e. boot scripts ]]
+
+local args = {...}
+
+if #args > 0 then
+	if args[1] == "install" then
+		print(package.installed.sysman.files[1].path)
+	elseif args[1] == "remove" then
+
+	end
+else
+	-- load API
+end
